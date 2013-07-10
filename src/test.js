448e3b0d6c1c846ab44fc1592ced23df23310402
Pascal Mainini <pascal.mainini@bfh.ch>
2013-07-10 17:31:11 +0200

    more hacking

--- src/test.js
+++ src/test.js
@@ -24,12 +24,29 @@
 
 var request = require('request'),
   querystring = require('querystring'),
-  filesystem = require('fs');
+  filesystem = require('fs'),
+  rdfstore = require('rdfstore');
 
 
 /***********************************************************
  * Function definitions
  **********************************************************/
+
+var _fetch = function _fetch(uri, accept, callback) {
+  if(uri.lastIndexOf('file', 0) === 0) {
+    filesystem.readFile(uri.substring(6), 'utf8', callback);
+  } else {
+    var options = {
+        method: 'GET',
+        uri: uri,
+        headers: {
+          'Accept': accept
+        }
+    };
+
+    request(options, callback);
+  }
+};
 
 var _fusekiQuery = function _fusekiQuery(query, accept, callback) {
   var options = {
@@ -38,8 +55,20 @@ var _fusekiQuery = function _fusekiQuery(query, accept, callback) {
       headers: {
         'Accept': accept,
         'Content-type': 'application/x-www-form-urlencoded'
-      } ,
+      },
       body: querystring.stringify({query: query})
+  };
+
+  request(options, callback);
+};
+
+var _fusekiPut = function _fusekiPut(graph, data, callback) {
+  var options = {
+      method: 'PUT',
+      uri: 'http://localhost:3030/uduvudu/data?' + querystring.stringify({graph: graph}),
+      headers: {'Content-type': 'text/turtle; charset=utf-8',
+                'Content-Length': data.length},
+      body: data
   };
 
   request(options, callback);
@@ -64,13 +93,37 @@ var _spinQuery = function _spinQuery(query, accept, callback) {
  * Main script
  **********************************************************/
 
-filesystem.readFile('../rdf/kitchen/purchaser/addobject.sparql', 'utf8', function _readErr (err, query) {
-  _fusekiQuery(query, 'text/turtle', function _receiveFuseki (error, response, data) {
-    _spinQuery(data, 'text/turtle', function _receiveSpin (error, response, data) {
-      _fusekiQuery(data, 'text/turtle', function _receiveFuseki (error, response, data) {
-        console.log(data);
+_fetch(process.argv[2], 'text/turtle', function _readErr (err, input) {
+  _fusekiQuery(input, 'text/turtle', function _receiveFuseki (error, response, data) {
+
+    _fusekiPut('http://test.me', data, function(err, response) {
+      console.log(err);
+      console.log(response);
       });
+/*
+    var store = new rdfstore.Store();
+    store.setPrefix('sp','http://spinrdf.org/sp');
+    store.setPrefix('rdfs', 'http://www.w3.org/2000/01/rdf-schema#');
+    store.setPrefix('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+
+    store.load('text/turtle', data, function(success, results) {
+
+    store.execute('SELECT * WHERE { ?s a sp:Construct ; (! :murks)* ?o . }', function(success, data) {
+//    store.execute('SELECT * WHERE { ?s ?p ?o . }', function(success, data) {
+      console.log(success);
+      console.log(data);
+    });
+
+    });
+
+*/
+
+
+
+
+//    _spinQuery(data, 'text/turtle', function _receiveSpin (error, response, data) {
+//      _fusekiQuery(data, 'text/turtle', function _receiveFuseki (error, response, data) {
+//     });
     });
   });
-});
 
