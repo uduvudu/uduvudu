95daec13f3d233536804d3cd9a9e0e93eed979c7
Michael Luggen <github.michael@oiu.ch>
2013-08-27 14:40:37 +0200

    first working

--- src/uduvudu.js
+++ src/uduvudu.js
@@ -9,21 +9,66 @@ function main(store) {
 };
 
 
+var matchFuncs = [
+    //NAME: title
+    function (graph) {
+        var proposal;
+        var query = 'SELECT * \
+                     FROM NAMED <inputgraph> { GRAPH <inputgraph> { ?s <http://purl.org/dc/terms/title> ?title} \
+                     FILTER(LANGMATCHES(LANG(?title), \"en\"))}';
+
+        graph.execute(query, function(success, results) {
+            if(success && (! _.isEmpty(results))) {
+                proposal = {elements: 1, context:{title: _.first(results).title.value}, template:{name: "title"}};
+            }
+            else
+            {
+                proposal = false;
+            }
+        
+        });
+        return proposal; 
+        },
+    //NAME: title, text
+    function (graph) {
+        var query = 'SELECT *\
+                     FROM NAMED <inputgraph> { GRAPH <inputgraph> { ?s <http://purl.org/dc/terms/title> ?title. ?s <http://rdfs.org/sioc/ns#content> ?text.} \
+                     FILTER(LANGMATCHES(LANG(?title), \"en\"))}';
+
+        graph.execute(query, function(success, results) {
+            if(success && (! _.isEmpty(results))) {
+                proposal = {elements: 2, context:{title: _.first(results).title.value, text: _.first(results).text.value}, template:{name: "text_title"}};
+            }
+            else
+            {
+                 proposal = false;
+            }
+        
+        });
+          return proposal;
+        },
+    //NAME: zero graph
+    function (graph) {
+        return false;
+        },
+    ];
+
+
+
 function matcher(inputGraph) {
 
-      var query = 'SELECT ?o \
-                     FROM NAMED <inputgraph> { GRAPH <inputgraph> { ?s <http://purl.org/dc/terms/title> ?o} \
-                     FILTER(LANGMATCHES(LANG(?o), \"en\"))}';
-
-      inputGraph.execute(query, function(success, results) {
-            console.log(results); 
-        });
-
-    return ([{context:{title: "test", content:"none"}, template:{name: "tempSimple"}}]);
+    var proposals = _.compact(_.map(matchFuncs, function (func){return func(inputGraph);}));
+    var sorted = _.sortBy(proposals, function (proposal) {return -proposal.elements;});
+    if( _.isEmpty(sorted)) {
+        return ([{context:{title: "none"}, template:{name: "title"}}]);
+    } else {
+        return [_.first(sorted)];
+    }
 };
 
 
 function visualizer(visuals) {
+    console.log(visuals);
     var output = "";
     _.each(visuals,
         function (visual){
