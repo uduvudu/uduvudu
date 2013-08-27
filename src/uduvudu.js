ca4e4d84ccb4d765fb308a3fd0678993d5a2129f
Michael Luggen <github.michael@oiu.ch>
2013-08-27 13:19:25 +0200

    first chain complete

--- src/uduvudu.js
+++ src/uduvudu.js
@@ -1,7 +1,22 @@
 
 
 function main() {
+    var visuals = matcher("graph");
+    var output = visualizer(visuals);
+    return output;
+};
 
 
-    return "html";
-}
+function matcher(inputGraph) {
+    return ([{context:{title: inputGraph, content:"none"}, template:{name: "tempSimple"}}]);
+};
+
+
+function visualizer(visuals) {
+    var output = "";
+    _.each(visuals,
+        function (visual){
+           output += "<h1>" + visual.context.title + "</h1>";
+        });
+    return output;
+};
