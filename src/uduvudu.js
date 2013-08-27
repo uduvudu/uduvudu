a13be5ba785cc40b34898a2d25bf0a9645d88d00
Michael Luggen <github.michael@oiu.ch>
2013-08-27 16:16:12 +0200

    got rid of en filter for now

--- src/uduvudu.js
+++ src/uduvudu.js
@@ -16,30 +16,10 @@ function main(store) {
  *
  */
 var matchFuncs = [
-    //NAME: title
-    function (graph) {
-        var proposal;
-        var query = 'SELECT * \
-                     FROM NAMED <inputgraph> { GRAPH <inputgraph> { ?s <http://purl.org/dc/terms/title> ?title} \
-                     FILTER(LANGMATCHES(LANG(?title), \"en\"))}';
-
-        graph.execute(query, function(success, results) {
-            if(success && (! _.isEmpty(results))) {
-                proposal = {elements: 1, context:{title: _.first(results).title.value}, template:{name: "title"}};
-            }
-            else
-            {
-                proposal = false;
-            }
-        
-        });
-        return proposal; 
-        },
     //NAME: title, text
     function (graph) {
         var query = 'SELECT *\
-                     FROM NAMED <inputgraph> { GRAPH <inputgraph> { ?s <http://purl.org/dc/terms/title> ?title. ?s <http://rdfs.org/sioc/ns#content> ?text.} \
-                     FILTER(LANGMATCHES(LANG(?title), \"en\"))}';
+                     FROM NAMED <inputgraph> { GRAPH <inputgraph> { ?s <http://purl.org/dc/terms/title> ?title. ?s <http://rdfs.org/sioc/ns#content> ?text.} }';
 
         graph.execute(query, function(success, results) {
             if(success && (! _.isEmpty(results))) {
@@ -53,8 +33,33 @@ var matchFuncs = [
         });
           return proposal;
         },
+    //NAME: title
+    function (graph) {
+        var proposal;
+        var query = 'SELECT * \
+                     FROM NAMED <inputgraph> { GRAPH <inputgraph> { ?s <http://purl.org/dc/terms/title> ?title} }';
+
+        graph.execute(query, function(success, results) {
+            if(success && (! _.isEmpty(results))) {
+                proposal = {elements: 1, context:{title: _.first(results).title.value}, template:{name: "title"}};
+            }
+            else
+            {
+                proposal = false;
+            }
+        
+        });
+        return proposal; 
+        },
     //NAME: zero graph
     function (graph) {
+        var query = 'SELECT *\
+                     FROM NAMED <inputgraph> { GRAPH <inputgraph> { ?s <http://purl.org/dc/terms/title> ?o.} }';
+        graph.execute(query, function(success, results) {
+            if(success && (! _.isEmpty(results))) {
+                console.log(results);
+            }
+        });
         return false;
         },
     ];
@@ -62,9 +67,9 @@ var matchFuncs = [
 
 
 function matcher(inputGraph) {
-
     var proposals = _.compact(_.map(matchFuncs, function (func){return func(inputGraph);}));
     var sorted = _.sortBy(proposals, function (proposal) {return -proposal.elements;});
+    console.debug("The sorted found proposals:",sorted)
     if( _.isEmpty(sorted)) {
         return ([{context:{title: "none"}, template:{name: "title"}}]);
     } else {
@@ -75,7 +80,6 @@ function matcher(inputGraph) {
 
 
 function visualizer(visuals) {
-    console.log(visuals);
     var output = "";
 
     _.each(visuals,
